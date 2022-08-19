b25930222ba135184fe712c9542287e572873a19
pedrolgois <pedro.gois.086@ufrn.edu.br>
2022-08-19 12:25:30 -0300

    criação da listagem dos usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import { UserItem } from './components/UserItem';
+
+export type userData = {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: any,
+    phone: string,
+    website: string,
+    company: any,
+}
+
+type todoData = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean,
+}
 
 function App() {
+    const [users, setUsers] = useState<userData[]>([]);
+    const [todos, setTodos] = useState<todoData[]>([]);
+
+    const listUsers = () => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res) => res.json())
+        .then((users) => {
+            setUsers(users);
+        });
+    }
+
+    const listTodos = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((res) => res.json())
+        .then((todos) => {
+            setTodos(todos);
+        });
+    }
+
+    useEffect(()=>{
+        listUsers();
+    },[])
+
+    useEffect(()=>{
+        console.log(users)
+    },[users])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+            <Col sm={6} className={'usersList'}>
+                {users.map((user) => {
+                    return(
+                        <UserItem user={user}/>
+                    )
+                })}
+            </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/UserItem/index.tsx
@@ -0,0 +1,23 @@
+// @flow 
+import * as React from 'react';
+import { userData } from '../../App';
+
+import styles from './index.module.css'
+
+type Props = {
+    user: userData
+};
+
+export const UserItem = (props: Props) => {
+    return (
+        <div className={styles.user}>
+            <div className={styles.icon}>{props.user.name[0]}</div>
+            <div className={styles.info}>
+                <h2>{props.user.name}</h2>
+                <small>{props.user.email}</small>
+            </div>
+            <div className={styles.acess}>
+            </div>
+        </div>
+    );
+};
